028a3e423b6d1dd1cc960d290614b79d033cedb3
Layton Barney <laytonb2@byu.edu>
2022-03-09 16:34:39 -0700

    Finished

---
+++ OnlineBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+
+namespace OnlineBookstore.Components
+{
+
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private ShoppingCart cart;
+        public CartSummaryViewComponent(ShoppingCart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
